8abe4601650baad7017f36ba8cb72b7861545c20
Tasedit <tasedit@gmail.com>
2025-04-01 19:33:15 -0600

    creating branch Mission13

--- backend/Mission11_Davis.API/obj/Debug/net8.0/Mission11_Davis.API.AssemblyInfo.cs
+++ backend/Mission11_Davis.API/obj/Debug/net8.0/Mission11_Davis.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Davis.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043d390ab0c33e97fc8912374876a2efafea7966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c1093b81673a1456f0b6d2fb9336172029616d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Davis.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Davis.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
